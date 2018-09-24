35f2830e30aa650373a7f54abdded5089aad5d24
hoanglocdn95 <hoanglocdn95@gmail.com>
2018-09-24 10:08:24 +0700

    24/9 10:08

--- to-do-redux/src/index.js
+++ to-do-redux/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers'
+import { Provider } from 'react-redux'
 import App from './components/App'
-​
-const store = createStore(todoApp)
-​
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
+
 render(
   <Provider store={store}>
     <App />
